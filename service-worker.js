7c02ec977f7030f95e8645cd9eaa69b5dce4697d
hippies67 <ikbal.malik26@gmail.com>
2022-11-08 13:22:43 +0700

    adding pwa to the app

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "SW-001";
-const toCache = ["/", "manifest.json", "assets/js/register.js", "logo.png"];
+const toCache = ["/", "manifest.json", "dist/js/register.js", "logo.png"];
 self.addEventListener("beforeinstallprompt", (e) => {
   e.preventDefault();
   deferredPrompt = e;
